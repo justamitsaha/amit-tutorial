6021032e43c9b907f3021eec300c9f34da33de4b
justamitsaha <justamitsaha@gmail.com>
2022-07-16 17:37:24 +0530

    JSON Changes

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,25 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.Random;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> lst = new ArrayList<>();
-        lst.add(5);
-        IntStream intStream = lst.stream().mapToInt(n -> n);
-        lst.add(9);
-        System.out.println(intStream.count());
-    }
-}
 
-abstract class another {
-    public abstract void test();
+        Optional<Double> opt = average();
+        opt.ifPresent(System.out::println); // 95.0
+
+    }
+
+    public static Optional<Double> average(int... scores) {
+        if (scores.length == 0)
+            return Optional.empty();
+        int sum = 0;
+        for (int score : scores)
+            sum += score;
+        return Optional.of((double) sum / scores.length);
+    }
 }
